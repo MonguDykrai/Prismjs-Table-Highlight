da66d537a16bcb48f09867d857798f9093254721
MonguDykrai <kisaihou@gmail.com>
2022-07-01 10:36:26 +0800

    1 file modified

--- src/Core.js
+++ src/Core.js
@@ -4,19 +4,94 @@ import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 
 const code = `
-  import React, { StrictMode } from 'react';
-  import { createRoot } from 'react-dom/client';
+import { Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/lib/table';
+import React from 'react';
 
-  import App from './App';
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+}
 
-  const rootElement = document.getElementById('root');
-  const root = createRoot(rootElement);
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
 
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+const App: React.FC = () => <Table columns={columns} dataSource={data} />;
+
+export default App;
 `;
 
 // const code = `
